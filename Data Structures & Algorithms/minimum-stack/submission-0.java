81981034bdbd263f0711018f481976ee7dce0372
BaoHo <baoho1503@gmail.com>
2026-05-16 20:47:21 +0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,34 @@
+class MinStack {
+    List<Integer> stack;
+    List<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayList<>();
+        minStack = new ArrayList<>();
+    }
+    
+    public void push(int val) {
+        stack.add(val);
+
+        if(minStack.isEmpty())
+        {
+            minStack.add(val);
+        }
+        else {
+            minStack.add(Math.min(val, minStack.get(minStack.size()-1)));
+        }
+    }
+    
+    public void pop() {
+        stack.remove(stack.size()-1);
+        minStack.remove(minStack.size()-1);
+    }
+    
+    public int top() {
+        return stack.get(stack.size()-1);
+    }
+    
+    public int getMin() {
+        return minStack.get(minStack.size()-1);
+    }
+}
